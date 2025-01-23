183249aae696dac3aea596864362e4299f872ee2
Loc Bui <locbuigia7101996@gmail.com>
2025-01-23 00:05:38 -0800

    Replaced a with Link in ProducType component

--- src/components/ProductType.jsx
+++ src/components/ProductType.jsx
@@ -7,37 +7,38 @@ import {
   BAG_TYPE_DUFFLE,
   BAG_TYPE_TRAVEL,
 } from "../constants";
+import { Link } from "react-router-dom";
 
 const ProductType = () => {
   return (
     <div className="w-full h-full grid grid-cols-1 sm:grid-cols-3 text-white tracking-widest">
-      <a
-        href={`/products?type=${BAG_TYPE_BACKPACK}`}
+      <Link
+        to={`/products?type=${BAG_TYPE_BACKPACK}`}
         className="relative w-full h-full"
       >
         <img className="w-full sm:h-full" src={backpackTypeImg} />
         <div className="absolute flex justify-center items-center bottom-0 w-full h-full text-[0px] hover:text-3xl hover:bg-black hover:bg-opacity-60 duration-500">
           <h1>BACKPACK</h1>
         </div>
-      </a>
-      <a
-        href={`/products?type=${BAG_TYPE_DUFFLE}`}
+      </Link>
+      <Link
+        to={`/products?type=${BAG_TYPE_DUFFLE}`}
         className="relative w-full h-full"
       >
         <img className="w-full sm:h-full" src={duffleTypeImg} />
         <div className="absolute flex justify-center items-center bottom-0 w-full h-full text-[0px] hover:text-3xl hover:bg-black hover:bg-opacity-60 duration-500">
           DUFFLE
         </div>
-      </a>
-      <a
-        href={`/products?type=${BAG_TYPE_TRAVEL}`}
+      </Link>
+      <Link
+        to={`/products?type=${BAG_TYPE_TRAVEL}`}
         className="relative w-full h-full"
       >
         <img className="w-full sm:h-full" src={travelTypeImg} />
         <div className="absolute flex justify-center items-center bottom-0 w-full h-full text-[0px] hover:text-3xl hover:bg-black hover:bg-opacity-60 duration-500">
           TRAVEL
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
